0ebb913b729c4d5a716ed8f215c3d6839f7f0e56
1991dinesh <55969220+1991dinesh@users.noreply.github.com>
2019-10-01 04:01:49 +0530

    without try catch and Exception

---
+++ exception3.java
@@ -0,0 +1,16 @@
+
+public class exception3 
+{
+
+	public static void main(String[] args)
+	{
+	   System.out.println(1);
+	   System.out.println(10/5);
+	   System.out.println(3);
+	   System.out.println(4);
+	   System.out.println(5);
+	   System.out.println(6);
+
+	}
+
+}
