f0672db5de9187969df4cd034e2677ff0779cd68
1991dinesh <55969220+1991dinesh@users.noreply.github.com>
2019-10-01 04:07:30 +0530

    Try catch execution without abnormal termination

---
+++ exception4.java
@@ -0,0 +1,24 @@
+
+public class exception4 
+{
+
+	public static void main(String[] args)
+	{
+	  System.out.println(1);
+	  System.out.println(2);
+	  try
+	  {
+		  System.out.println(3);
+		  int a=200 , b=2;
+		  System.out.println(a/b);
+		  System.out.println(4);
+	  }
+	  catch(Exception e)
+	  {
+		  System.out.println(5);
+	  }
+	  System.out.println(6);
+
+	}
+
+}
