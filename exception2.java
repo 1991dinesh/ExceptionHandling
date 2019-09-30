e05555f3d6dfe91b3dd5861154d14e60a69237e9
1991dinesh <55969220+1991dinesh@users.noreply.github.com>
2019-10-01 03:57:14 +0530

    without trycatch

---
+++ exception2.java
@@ -0,0 +1,15 @@
+//Application with out try catch
+public class exception2 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(1);    //After line 7 data is not getting printed only 1 will print
+		System.out.println(10/0);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+
+	}
+
+}
